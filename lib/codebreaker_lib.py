2d883427334a3edcb0e760f9b47ba73f266f6b46
robertpitchford <bitbucket@pitchfords.net>
2012-07-08 22:25:12 -0700

    Added use of reduce (similar to inject in Ruby)

--- lib/codebreaker_lib.py
+++ lib/codebreaker_lib.py
@@ -8,27 +8,25 @@ class codebreaker(object):
         self.secret = secret
 
     def exact_match_count(self, guess):
-        count = 0
-        for i in range(0, 4):
-            if guess[i] == self.secret[i]:
-                count += 1
-        return count
+        return reduce (lambda count, i: count + (1 if guess[i] == self.secret[i] else 0), range(0,4), 0)
 
     def number_match_count(self, guess):
         return self.total_match_count(guess) - self.exact_match_count(guess)
 
+    def total_match_count(self, guess):
+        secret = list(self.secret)
+        return reduce(lambda count, g: count + (1 if self.delete_first(g, secret) else 0), guess, 0)
+
+    def delete_first(self, g, secret):
+        try:
+            secret.remove(g)
+            return True
+        except Exception:
+            return False
+
+
     def delete_char_at(self, pos, secret):
         return secret[:pos] + secret[pos + 1:]
-
-    def total_match_count(self, guess):
-        count = 0
-        secret = self.secret
-        for i in range(0, 4):
-            pos = secret.find(guess[i])
-            if pos != -1:
-                secret = self.delete_char_at(pos, secret)
-                count += 1
-        return count
 
     def is_number_match(self, guess, i):
         return guess[i] in self.secret
