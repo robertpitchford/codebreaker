acd49c3753767a1548978874e3d9d84d76f08b9a
robertpitchford <bitbucket@pitchfords.net>
2012-07-10 16:49:24 -0700

    Added test to check incorrect guess does not end the game

--- test/robot/steps/codebreaker_steps.py
+++ test/robot/steps/codebreaker_steps.py
@@ -23,6 +23,9 @@ def I_start_a_new_game():
 def I_should_see(expected):
     verify(state.output).write(contains(expected))
 
+def I_should_not_see(expected):
+    verify(state.output, never).write(contains(expected))
+
 def the_secret_code(secret):
     reset_state()
     state.output = mock()
@@ -44,10 +47,16 @@ def codebreaker_is_started():
     state.wrapper.generate_secret = lambda: state.SECRET
     state.wrapper.new_game()
 
-
 def I_guess_the_correct_code():
     when(state.input).readline().thenReturn(state.SECRET + "\n")
     state.wrapper.next_guess()
 
+def I_guess_an_incorrect_code():
+    when(state.input).readline().thenReturn("5555\n")
+    state.wrapper.next_guess()
+
 def the_game_should_end():
     assert state.wrapper.isPlaying == False
+
+def the_game_should_not_end():
+    assert state.wrapper.isPlaying == True
