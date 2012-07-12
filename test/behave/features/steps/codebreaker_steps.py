e40667eb93ca93b01a2665625e7db8a87250d71a
robertpitchford <bitbucket@pitchfords.net>
2012-07-12 08:58:28 -0700

    Removed wildcard imports from behave steps

--- test/behave/features/steps/codebreaker_steps.py
+++ test/behave/features/steps/codebreaker_steps.py
@@ -1,69 +1,69 @@
 ##noinspection PyUnresolvedReferences
 import test_helper
-from behave import *
-import mockito
+from mockito import mock, verify, contains, never, when
+import behave
 import codebreaker_lib
 import codebreaker_wrapper
 
-@given(u'codebreaker is not running')
+@behave.given(u'codebreaker is not running')
 def given_codebreaker_is_not_running(context):
     pass
 
-@when(u'I start a new game')
+@behave.when(u'I start a new game')
 def when_i_start_a_new_game(context):
-    context.output = mockito.mock()
-    input = mockito.mock()
+    context.output = mock()
+    input = mock()
     codebreaker_wrapper.codebreaker_wrapper(input, context.output)
 
-@then(u'I should see "{msg}"')
+@behave.then(u'I should see "{msg}"')
 def then_i_should_see_msg(context, msg):
-    mockito.verify(context.output).write(mockito.contains(msg))
+    verify(context.output).write(contains(msg))
 
-@then(u'I should not see "{msg}"')
+@behave.then(u'I should not see "{msg}"')
 def I_should_not_see(context, msg):
-    mockito.verify(context.output, mockito.never).write(mockito.contains(msg))
+    verify(context.output, never).write(contains(msg))
 
-@given(u'the secret code "{secret}"')
+@behave.given(u'the secret code "{secret}"')
 def the_secret_code(context, secret):
-    context.output = mockito.mock()
+    context.output = mock()
     context.cb = codebreaker_lib.codebreaker(context.output)
     context.cb.start(secret)
 
-@when(u'I guess "{guess}"')
+@behave.when(u'I guess "{guess}"')
 def I_guess(context, guess):
     context.cb.guess(guess)
 
-@then(u'the mark should be "{mark}"')
+@behave.then(u'the mark should be "{mark}"')
 def the_mark_should_be_mark(context, mark):
-    mockito.verify(context.output).write(mark + "\n")
+    verify(context.output).write(mark + "\n")
 
-@then(u'the mark should be ""')
+@behave.then(u'the mark should be ""')
 def the_mark_should_be(context):
     the_mark_should_be_mark(context, "")
 
-@given(u'codebreaker is started')
+@behave.given(u'codebreaker is started')
 def codebreaker_is_started(context):
     context.SECRET = "1234"
-    context.input = mockito.mock()
-    context.output = mockito.mock()
+    context.input = mock()
+    context.output = mock()
     context.wrapper = codebreaker_wrapper.codebreaker_wrapper(context.input, context.output)
     context.wrapper.generate_secret = lambda: context.SECRET
     context.wrapper.new_game()
 
-@when(u'I guess the correct code')
+@behave.when(u'I guess the correct code')
 def I_guess_the_correct_code(context):
-    mockito.when(context.input).readline().thenReturn(context.SECRET + "\n")
+    when(context.input).readline().thenReturn(context.SECRET + "\n")
     context.wrapper.next_guess()
 
-@when(u'I guess an incorrect code')
+@behave.when(u'I guess an incorrect code')
 def I_guess_an_incorrect_code(context):
-    mockito.when(context.input).readline().thenReturn("5555\n")
+    when(context.input).readline().thenReturn("5555\n")
     context.wrapper.next_guess()
 
-@then(u'the game should end')
+@behave.then(u'the game should end')
 def the_game_should_end(context):
     assert context.wrapper.isPlaying == False
 
-@then(u'the game should not end')
+@behave.then(u'the game should not end')
 def the_game_should_not_end(context):
     assert context.wrapper.isPlaying == True
